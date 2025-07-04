19a54f8e8887d22f94c96ced358072386890d3ed
jaredblumer <jaredblumer@protonmail.com>
2025-07-04 15:06:18 -0400

    Remove trailing whitespace

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,7 +19,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def check_captcha
   self.resource = resource_class.new(sign_in_params)
-  
+
   result = verify_recaptcha(model: resource)
 
   unless result
